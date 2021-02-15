6f0223eb49eb6a3ef6d0ed56ff53ea79d275dbc6
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-15 17:58:14 +0000

    Add MaterialUI core and add basic styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { CssBaseline, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+
+    backgroundColor: '#202430',
+    color: 'whitesmoke',
+
+    paddingTop: '100px',
+    minHeight: '100vh',
+    width: '100%',
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
     <>
-      <h1>hello world</h1>
+      <CssBaseline />
+
+      <div className={classes.root}>
+        <h1>hello world</h1>
+      </div>
     </>
   );
 }
